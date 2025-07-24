6d48a3673589fac5af0d4b3a776f984b9a4c86ab
Nehagoindani <73845534+Nehagoindani@users.noreply.github.com>
2025-07-24 17:07:19 +1000

    init

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import SignInPage from "./pages/SignInPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import ForgotPassword from "./pages/forgotPass.jsx";
+import ResetPass from "./pages/resetPass.jsx";
+import Home from "./pages/Home.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/" element={<App />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+       <Route path="/forgotPass" element={<ForgotPassword />} />
+          <Route path="/resetPass" element={<ResetPass />} />
+             <Route path="/home" element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
